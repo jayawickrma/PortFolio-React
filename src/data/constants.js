5b9fd4b4ef830980bf48ea581b8a603a0335ef77
jayawickrama <nishantharaka2004@gmail.com>
2025-03-10 22:15:54 +0530

    update

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,12 +5,11 @@ export const Bio = {
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/rishavchanda",
+  github: "https://github.com/jayawickrma",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-  twitter: "https://twitter.com/RishavChanda",
-  insta: "https://www.instagram.com/rishav_chanda/",
+    "https://drive.google.com/file/d/1cwtg71EvXTvWrluy1u9Xj1HU20509YSn/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/nishan-tharaka-7b09a52ab/",
+  instagram: "https://www.instagram.com/rishav_chanda/",
   facebook: "https://www.facebook.com/rishav.chanda.165/",
 };
 
